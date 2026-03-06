54cc81e09326efd6092cee72028fefb59c4d4c6b
Soyalchawhan <soyalchawhan@gmail.com>
2026-03-06 14:55:29 +0530

    Refactor AuditLog model to use ES6 imports

--- models/AuditLog.js
+++ models/AuditLog.js
@@ -1,51 +1,22 @@
-const mongoose = require('mongoose');
+import mongoose from 'mongoose'
 
 const auditLogSchema = new mongoose.Schema({
-  document: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: 'Document',
-    required: true
-  },
+  document: { type: mongoose.Schema.Types.ObjectId, ref: 'Document', required: true },
   action: {
     type: String,
     enum: [
-      'document_uploaded',
-      'document_viewed',
-      'signature_placed',
-      'share_link_generated',
-      'signing_started',
-      'document_signed',
-      'document_rejected',
-      'signed_pdf_generated',
-      'document_downloaded'
+      'document_uploaded', 'document_viewed', 'signature_placed',
+      'share_link_generated', 'signing_started', 'document_signed',
+      'document_rejected', 'signed_pdf_generated', 'document_downloaded'
     ],
     required: true
   },
-  actor: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: 'User',
-    default: null
-  },
-  actorName: {
-    type: String,
-    default: 'Anonymous'
-  },
-  actorEmail: {
-    type: String,
-    default: null
-  },
-  ipAddress: {
-    type: String,
-    default: null
-  },
-  userAgent: {
-    type: String,
-    default: null
-  },
-  metadata: {
-    type: mongoose.Schema.Types.Mixed,
-    default: {}
-  }
-}, { timestamps: true });
+  actor: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
+  actorName: { type: String },
+  actorEmail: { type: String },
+  ipAddress: { type: String },
+  userAgent: { type: String },
+  metadata: { type: mongoose.Schema.Types.Mixed }
+}, { timestamps: true })
 
-module.exports = mongoose.model('AuditLog', auditLogSchema);
+export default mongoose.model('AuditLog', auditLogSchema)
